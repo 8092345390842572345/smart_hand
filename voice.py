63e4435e9e02a27ace0211733c83e150ee29c171
f1rA5h <57868199+f1rA5h@users.noreply.github.com>
2020-07-21 23:36:11 +0300

    Add files via upload

---
+++ voice.py
@@ -0,0 +1,7 @@
+# I don't know what's the hell is that and how it works but 
+# if you have questions it's my email:
+#   --------------------------
+#   | ashtl.f1ra5h@gmail.com |
+#   --------------------------
+
+from pocketsphinx import i dont know what
