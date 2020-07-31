97ad2daed593eb975c6d14cf8e7bc4cb8f8fbd0f
f1rA5h <57868199+f1rA5h@users.noreply.github.com>
2020-07-31 16:22:35 +0300

    Add files via upload

--- voice.py
+++ voice.py
@@ -4,4 +4,23 @@
 #   | ashtl.f1ra5h@gmail.com |
 #   --------------------------
 
-from pocketsphinx import i dont know what
+import os
+from x import LiveSpeech, get_model_path
+
+model_path = get_model_path()
+
+speech = LiveSpeech(
+    verbose=False,
+    sampling_rate=16000,
+    buffer_size=2048,
+    no_search=False,
+    full_utt=False,
+    hmm=os.path.join(model_path, 'zero_ru.cd_cont_4000'),
+    lm=os.path.join(model_path, 'ru.lm'),
+    dic=os.path.join(model_path, 'ru.dic')
+)
+
+print("Say something!")
+
+for phrase in speech:
+    print(phrase)
